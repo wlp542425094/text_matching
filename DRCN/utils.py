3196493a1f99397d8271784af364b77c39ed15ba
leipeng <40786208+wlp542425094@users.noreply.github.com>
2019-12-10 20:21:05 +0800

    Add files via upload

---
+++ DRCN/utils.py
@@ -0,0 +1,223 @@
+import os
+import numpy as np
+import pandas as pd
+def shuffle(*arrs):
+    """ shuffle
+    Shuffle 数据
+    Arguments:
+        *arrs: 数组数据
+    Returns:
+        shuffle后的数据
+    """
+    arrs = list(arrs)
+    for i, arr in enumerate(arrs):
+        assert len(arrs[0]) == len(arrs[i])
+        arrs[i] = np.array(arr)
+    p = np.random.permutation(len(arrs[0]))
+    return tuple(arr[p] for arr in arrs)
+
+
+# 加载char_index训练数据
+def load_char_data(file, data_size=None):
+    path = os.path.join(os.path.dirname(__file__), '../' + file)
+    df = pd.read_csv(path)
+    p = df['sentence1'].values[0:data_size]
+    h = df['sentence2'].values[0:data_size]
+    label = df['label'].values[0:data_size]
+
+    p, h, label = shuffle(p, h, label)
+
+    # [1,2,3,4,5] [4,1,5,2,0]
+    p_c_index, h_c_index = char_index(p, h)
+
+    return p_c_index, h_c_index, label
+
+# 字->index
+def char_index(p_sentences, h_sentences):
+    word2idx, idx2word = load_char_vocab()
+
+    p_list, h_list = [], []
+    for p_sentence, h_sentence in zip(p_sentences, h_sentences):
+        p = [word2idx[word.lower()] for word in p_sentence if len(word.strip()) > 0 and word.lower() in word2idx.keys()]
+        h = [word2idx[word.lower()] for word in h_sentence if len(word.strip()) > 0 and word.lower() in word2idx.keys()]
+
+        p_list.append(p)
+        h_list.append(h)
+
+    # 充填  句子长度为15
+    p_list = pad_sequences(p_list, maxlen=15)
+    h_list = pad_sequences(h_list, maxlen=15)
+
+    return p_list, h_list
+
+# 加载字典
+def load_char_vocab():
+    # path = os.path.join(os.path.dirname(__file__), 'text_matching\input\\vocab.txt')
+    path = "../input/vocab.txt"
+    vocab = [line.strip() for line in open(path, encoding='utf-8').readlines()]
+    word2idx = {word: index for index, word in enumerate(vocab)}
+    idx2word = {index: word for index, word in enumerate(vocab)}
+    return word2idx, idx2word
+
+def pad_sequences(sequences, maxlen=None, dtype='int32', padding='post',
+                  truncating='post', value=0.):
+    """ pad_sequences
+
+    把序列长度转变为一样长的，如果设置了maxlen则长度统一为maxlen，如果没有设置则默认取
+    最大的长度。填充和截取包括两种方法，post与pre，post指从尾部开始处理，pre指从头部
+    开始处理，默认都是从尾部开始。
+
+    Arguments:
+        sequences: 序列
+        maxlen: int 最大长度
+        dtype: 转变后的数据类型
+        padding: 填充方法'pre' or 'post'
+        truncating: 截取方法'pre' or 'post'
+        value: float 填充的值
+
+    Returns:
+        x: numpy array 填充后的序列维度为 (number_of_sequences, maxlen)
+
+    """
+    lengths = [len(s) for s in sequences]
+
+    nb_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    x = (np.ones((nb_samples, maxlen)) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if len(s) == 0:
+            continue  # empty list was found
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncating == 'post':
+            trunc = s[:maxlen]
+        else:
+            raise ValueError("Truncating type '%s' not understood" % padding)
+
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            raise ValueError("Padding type '%s' not understood" % padding)
+    return x
+
+import jieba
+import re
+from text_matching.diin import args
+
+# 加载词典
+def load_word_vocab():
+    path = os.path.join(os.path.dirname(__file__), 'word_vocab.tsv')
+    vocab = [line.strip() for line in open(path, encoding='utf-8').readlines()]
+    word2idx = {word: index for index, word in enumerate(vocab)}
+    idx2word = {index: word for index, word in enumerate(vocab)}
+    return word2idx, idx2word
+
+# 词->index
+def word_index(p_sentences, h_sentences):
+    word2idx, idx2word = load_word_vocab()
+
+    p_list, h_list = [], []
+    for p_sentence, h_sentence in zip(p_sentences, h_sentences):
+        p = [word2idx[word.lower()] for word in p_sentence if len(word.strip()) > 0 and word.lower() in word2idx.keys()]
+        h = [word2idx[word.lower()] for word in h_sentence if len(word.strip()) > 0 and word.lower() in word2idx.keys()]
+
+        p_list.append(p)
+        h_list.append(h)
+
+    p_list = pad_sequences(p_list, maxlen=args.max_char_len)
+    h_list = pad_sequences(h_list, maxlen=args.max_char_len)
+
+    return p_list, h_list
+
+# 加载char_index与动态词向量的训练数据
+def load_char_word_dynamic_data(path, data_size=None):
+    df = pd.read_csv(path)
+    p = df['sentence1'].values[0:data_size]
+    h = df['sentence2'].values[0:data_size]
+    label = df['label'].values[0:data_size]
+
+    p, h, label = shuffle(p, h, label)
+
+    p_char_index, h_char_index = char_index(p, h)
+
+    p_seg = map(lambda x: list(jieba.cut(re.sub("[！，。？、~@#￥%&*（）.,:：|/`()_;+；…\\\\\\-\\s]", "", x))), p)
+    h_seg = map(lambda x: list(jieba.cut(re.sub("[！，。？、~@#￥%&*（）.,:：|/`()_;+；…\\\\\\-\\s]", "", x))), h)
+
+    p_word_index, h_word_index = word_index(p_seg, h_seg)
+
+    return p_char_index, h_char_index, p_word_index, h_word_index, label
+
+
+
+from gensim.models import Word2Vec
+
+# 静态w2v
+def w2v(word, model):
+    try:
+        return model.wv[word]
+    except:
+        return np.zeros(args.word_embedding_len)
+
+def w2v_process(vec):
+    if len(vec) > args.max_word_len:
+        vec = vec[0:args.max_word_len]
+    elif len(vec) < args.max_word_len:
+        zero = np.zeros(args.word_embedding_len)
+        length = args.max_word_len - len(vec)
+        for i in range(length):
+            vec = np.vstack((vec, zero))
+    return vec
+
+# 加载char_index、静态词向量、动态词向量的训练数据
+def load_all_data(path, data_size=None):
+    model = Word2Vec.load('word2vec.model')
+    df = pd.read_csv(path)
+    p = df['sentence1'].values[0:data_size]
+    h = df['sentence2'].values[0:data_size]
+    label = df['label'].values[0:data_size]
+
+    p, h, label = shuffle(p, h, label)
+
+    p_c_index, h_c_index = char_index(p, h)
+
+    p_seg = list(map(lambda x: list(jieba.cut(re.sub("[！，。？、~@#￥%&*（）.,:：|/`()_;+；…\\\\\\-\\s]", "", x))), p))
+    h_seg = list(map(lambda x: list(jieba.cut(re.sub("[！，。？、~@#￥%&*（）.,:：|/`()_;+；…\\\\\\-\\s]", "", x))), h))
+
+    p_w_index, h_w_index = word_index(p_seg, h_seg)
+
+    p_seg = map(lambda x: list(jieba.cut(x)), p)
+    h_seg = map(lambda x: list(jieba.cut(x)), h)
+
+    p_w_vec = list(map(lambda x: w2v(x, model), p_seg))
+    h_w_vec = list(map(lambda x: w2v(x, model), h_seg))
+
+    p_w_vec = list(map(lambda x: w2v_process(x), p_w_vec))
+    h_w_vec = list(map(lambda x: w2v_process(x), h_w_vec))
+
+    # 判断是否有相同的词
+    same_word = []
+    for p_i, h_i in zip(p_w_index, h_w_index):
+        dic = {}
+        for i in p_i:
+            if i == 0:
+                break
+            dic[i] = dic.get(i, 0) + 1
+        for index, i in enumerate(h_i):
+            if i == 0:
+                same_word.append(0)
+                break
+            dic[i] = dic.get(i, 0) - 1
+            if dic[i] == 0:
+                same_word.append(1)
+                break
+            if index == len(h_i) - 1:
+                same_word.append(0)
+
+    return p_c_index, h_c_index, p_w_index, h_w_index, p_w_vec, h_w_vec, same_word, label
+
+if __name__ == '__main__':
+    load_all_data('../input/train.csv', data_size=1000)
